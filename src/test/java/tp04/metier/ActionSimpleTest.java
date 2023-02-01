154b611776b3d7c82585d819c04a8a0910f16051
andyb <andyb@Andy>
2023-02-01 15:18:25 +0100

    Andy - Test unitaire

--- src/test/java/tp04/metier/ActionSimpleTest.java
+++ src/test/java/tp04/metier/ActionSimpleTest.java
@@ -1,9 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+* Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 package tp04.metier;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,12 +13,17 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author andyb
  */
 public class ActionSimpleTest {
-    
+    private static final String EXPECTED_LIBELLE = "Toto";
+
     public ActionSimpleTest() {
     }
 
-    @org.junit.jupiter.api.Test
-    public void testSomeMethod() {
+    @Test
+    public void testGetLibelleShouldPass() {
+        final ActionSimple as1 = new ActionSimple(EXPECTED_LIBELLE);
+        final String result = as1.getLibelle();
+        
+        Assertions.assertSame(EXPECTED_LIBELLE, result, "The label must be the same as the one used at creation time.");
     }
     
 }
